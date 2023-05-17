c117f5f4a1960ce38b85665dfcb634a2f071227e
yurrrri <leeyuri.andapp@gmail.com>
2023-05-17 10:26:53 +0000

    지난 문제 복습

--- Baekjoon/16234.swift
+++ Baekjoon/16234.swift
@@ -1,56 +1,65 @@
 import Foundation
 
-let input = readLine()!.split(separator:" ").map { Int(String($0))! }
-let n = input[0], l = input[1], r = input[2]
+let nlr = readLine()!.split(separator:" ").map { Int(String($0))! }
+let n = nlr[0], l = nlr[1], r = nlr[2]
 
-var board:[[Int]] = []
+let dx = [-1, 1, 0, 0]
+let dy = [0, 0, -1, 1]
 
-var dx = [-1, 1, 0, 0]
-var dy = [0, 0, -1, 1]
 var nx = 0
 var ny = 0
 
+var board:[[Int]] = []
 for _ in 0..<n {
   board.append(readLine()!.split(separator:" ").map { Int(String($0))! })
 }
-var visited = Array(repeating: Array(repeating: false, count: n), count: n) //중복으로 연합으로 묶지 않기 위한 visited 배열
 
-func bfs(_ i:Int, _ j:Int) -> Int {
-  var q = [(i, j)]
-  var union = [(i, j)] //연합된 나라 리스트
+var visited:[[Bool]] = []
 
-  var sum = board[i][j] // 연합된 나라의 인구수 총합
-  visited[i][j] = true //맨 처음 시작점은 방문처리
-  
-  while !q.isEmpty {
-    let (x, y) = q.removeFirst()
+func bfs(_ x: Int, _ y: Int) -> Int {
+  var q = [(x, y)]
+  visited[x][y] = true // visited를 체크하는 이유는 예를들어 50-30 과 30-50 결과가 같으므로 체크해야함
+  var union = [(x, y)]
+
+  var sum = board[x][y]
+  var idx = 0
+
+  while idx < q.count {
+    let (x, y) = q[idx]
 
     for i in 0..<4 {
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if 0..<n ~= nx && 0..<n ~= ny && l...r ~= abs(board[nx][ny] - board[x][y]) && !visited[nx][ny] {
+      guard 0..<n ~= nx && 0..<n ~= ny && !visited[nx][ny] else { continue }
+
+      if l...r ~= abs(board[nx][ny]-board[x][y]) { // 차이가 l부터 r 이하인 그래프만 탐색해야함 
         visited[nx][ny] = true
-        sum += board[nx][ny]
+        
         union.append((nx, ny))
         q.append((nx, ny))
+        sum += board[nx][ny]
       }
     }
+    idx += 1
   }
 
-  let unionPeopleCount = Int(floor(Double(sum) / Double(union.count))) //연합된 나라는 모두 (총합 / 연합 수)이며 소수점은 버림
+  // 탐색이 끝났으면
+  let count = sum / union.count
   for (i, j) in union {
-    board[i][j] = unionPeopleCount
+    board[i][j] = count
   }
-  
   return union.count
 }
 
-var answer = 0 // 며칠동안 인구이동이 발생했는가
+var answer = 0
 
-while true {
-  visited = Array(repeating: Array(repeating: false, count: n), count: n) //다시 연합으로 묶는 루프를 실행해야하므로 visited 초기화해야함
-  var flag = false //인구 이동이 발생했는지에 대한 플래그 변수
+while true { 
+  // while true로 감싸는 이유 -> "더 이상 아래 방법에 의해 인구 이동이 없을 때까지 지속됨"
+  // 64 ~ 72줄 까지 자체가 처음부터 bfs 돌면서 인구이동이 일어나는지 확인하는 부분
+  visited = Array(repeating: Array(repeating: false, count: n), count:n) // visited 초기화한 후 bfs 돈다음 인구이동 여부 확인
+  var flag = false
+  
   for i in 0..<n {
     for j in 0..<n {
       if !visited[i][j] {
--- Baekjoon/2615.swift
+++ Baekjoon/2615.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 var board:[[Int]] = []
-var dx = [0, 1, 1, -1] //차례대로 가로, 세로, 하향 대각선, 상향 대각선
-var dy = [1, 0, 1, 1]
+let dx = [0, 1, 1, -1] //차례대로 가로, 세로, 하향 대각선, 상향 대각선
+let dy = [1, 0, 1, 1]
+
+var nx = 0
+var ny = 0
 
 for _ in 0..<19 {
   board.append(readLine()!.split(separator:" ").map { Int(String($0))! })
@@ -15,18 +18,19 @@ for x in 0..<19 {
             
             for i in 0..<4 {
                 var count = 1
-                var nx = x + dx[i] //가로, 세로, 대각선 4방향 차례대로 확인
-                var ny = y + dy[i]
+              
+                nx = x + dx[i] //가로, 세로, 대각선 4방향 차례대로 확인
+                ny = y + dy[i]
                 
-                while 0<=nx && nx<19 && 0<=ny && ny<19 && board[nx][ny] == item { //item이 연속될 때까지 count
+                while 0..<19 ~= nx && 0..<19 ~= ny && board[nx][ny] == item { //item이 연속될 때까지 count
                     count += 1
                     
                     if count == 5 { //오목이면, 육목인지 아닌지 확인
-                        if 0 <= x-dx[i] && x-dx[i] < 19 && 0<=y-dy[i] && y-dy[i] < 19 && board[x-dx[i]][y-dy[i]] == item {
+                        if 0..<19 ~= x-dx[i] && 0..<19 ~= y-dy[i] && board[x-dx[i]][y-dy[i]] == item {
                             break //육목이면 반복문을 벗어나 그 다음 스텝 실행
                         }
                         
-                        if 0 <= nx+dx[i] && nx+dx[i] < 19 && 0<=ny+dy[i] && ny+dy[i] < 19 && board[nx+dx[i]][ny+dy[i]] == item {
+                        if 0..<19 ~= nx+dx[i] && 0..<19 ~= ny+dy[i] && board[nx+dx[i]][ny+dy[i]] == item {
                             break
                         }
                         
@@ -35,7 +39,7 @@ for x in 0..<19 {
                         exit(0)
                     }
                     
-                    nx += dx[i]
+                    nx += dx[i] // 가로면 가로 세로면 세로 방향대로 쭉 같은 방향으로 전진
                     ny += dy[i]
                 }
             }
