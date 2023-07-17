3bfe19cd50d953e3dbdcdf25e9e07c55c3a1f2f0
yurrrri <leeyuri.iosapp@gmail.com>
2023-07-17 14:52:53 +0000

    15663

--- Baekjoon/15663.swift
+++ Baekjoon/15663.swift
@@ -8,20 +8,18 @@ var answer:Set<[Int]> = []
 
 func permu(_ depth: Int, _ arr: [Int]) {
   if depth == m {
-    if !answer.contains(arr) { // array를 set로 바꿔서 시간초과 해결 -> contains가 O(n)이 아닌 O(1)
-       answer.insert(arr)
-      print(arr.map { String($0) }.joined(separator:" "))
-    }
+    answer.insert(arr)
     return
   }
 
   for i in 0..<n {
     if !visited[i] {
       visited[i] = true
-      permu(depth+1, arr + [nums[i]])
+      permu(depth+1, arr + nums[i])
       visited[i] = false
     }
   }
 }
 
-permu(0, [])
+print(arr)
+
